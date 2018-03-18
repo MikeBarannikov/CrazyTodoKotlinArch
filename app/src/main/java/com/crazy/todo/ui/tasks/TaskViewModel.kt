7360eda1bcab9905a70ae9849072b45bccd74fd4
Mike <medan1989@gmail.com>
2018-03-18 12:31:28 +0300

    Added kotlin coroutines

--- app/src/main/java/com/crazy/todo/ui/tasks/TaskViewModel.kt
+++ app/src/main/java/com/crazy/todo/ui/tasks/TaskViewModel.kt
@@ -1,35 +1,12 @@
 package com.crazy.todo.ui.tasks
 
 import android.arch.lifecycle.ViewModel
+import kotlinx.coroutines.experimental.android.UI
+import kotlinx.coroutines.experimental.async
 
 class TaskViewModel: ViewModel() {
-    var a: Int? = null
-    var b: (() -> Unit)? = null
-
     fun test() {
-        myInline {
-            a = 100
-            return
+        async(UI) {
         }
-        myNoInline {
-            a = 200
-        }
-        myCrossInline {
-            a = 300
-        }
-    }
-
-    inline fun myInline(block: () -> Unit) {
-        block()
-    }
-
-    @Suppress("NOTHING_TO_INLINE")
-    inline fun myNoInline(noinline block: () -> Unit) {
-        val a = block
-        a()
-    }
-
-    inline fun myCrossInline(crossinline block: () -> Unit) {
-        block()
     }
 }
